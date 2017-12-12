600f4be3c2671f5e14e254de9282933acb34c67c
Emp-Norton <jamesjelenko@gmail.com>
2017-12-12 12:30:19 -0800

    Effectively fetching from server, need to implement search + re-render.

--- src/collections/videos.js
+++ src/collections/videos.js
@@ -9,10 +9,18 @@ var Videos = Backbone.Collection.extend({
     $.ajax({
       url: 'https://www.googleapis.com/youtube/v3/search',
       type: 'GET',
-      data: {'maxResults': '25',
+      data: {'maxResults': '5',
+        key: window.YOUTUBE_API_KEY,
         part: 'snippet',
         q: query,
-        type: ''}
+        type: ''},
+      success: function(data) {
+        window.videos = data;
+      },
+      error: function(data) {
+        console.error("Failed", data);
+      } 
+     
     });
   }  
 
