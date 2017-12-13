5f7072993dbf8ccd0dde300a0c2f0b16c4bdb2ff
James Jelenko <jamesjelenko@gmail.com>
2017-12-12 19:15:46 -0800

    refactored with listenTo and selectVideo functions

--- src/views/videoPlayer.js
+++ src/views/videoPlayer.js
@@ -2,13 +2,17 @@ var VideoPlayerView = Backbone.View.extend({
 
   el: '.player',
 
+  initialize: function() {
+    this.listenTo(this.collection, 'select', this.selectVideo);
+  },
+
+  selectVideo: function(selection) {
+    this.model = selection;
+    this.render();
+  },
+  
   render: function() {
-    if (this.model) {
-      this.$el.html(this.template(this.model.attributes));
-    } else {
-      this.$el.html('<div class="loading">Please wait...THIS IS THE PLAYER VIEW</div>');
-    }
-   
+    this.model ? this.$el.html(this.template(this.model.attributes)) : this.$el.html('<div class="loading">Please wait...</div>');
     return this;
   },
 
