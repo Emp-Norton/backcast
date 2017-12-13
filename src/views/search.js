cec138ad80e740dd93ab36c684cf97933e61a8de
James Jelenko <jamesjelenko@gmail.com>
2017-12-12 19:13:27 -0800

    refactored to use backbone fetch

--- src/views/search.js
+++ src/views/search.js
@@ -2,11 +2,22 @@ var SearchView = Backbone.View.extend({
 
   el: '.search',
 
+  events: {
+    'click button': 'performSearch',
+  },
+
+  performSearch: function() {
+    var query = this.$('input').val();
+    if (query) {
+      this.collection.search(query);
+    }
+    this.$('input').val('');
+  },
+  
   render: function() {
     this.$el.html(this.template());
     return this;
   },
 
   template: templateURL('src/templates/search.html')
-
 });
